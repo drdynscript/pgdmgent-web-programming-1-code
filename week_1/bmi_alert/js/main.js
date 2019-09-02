97c0df0f123df79d35333b000361478c3584c938
Philippe De Pauw - Waterschoot <drdynscript@gmail.com>
2019-09-02 21:31:39 +0200

    Add examples

---
+++ week_1/bmi_alert/js/main.js
@@ -0,0 +1,4 @@
+const length = prompt("Please enter your length", 1.72);
+const weight = prompt("Please enter your weight", 65);
+const bmi = weight/(length*length);
+alert(`Your bmi is ${bmi}`);
---
+++ week_1/personal_informantion/index.js
@@ -0,0 +1,23 @@
+const firstName = 'Philippe';
+const surName = 'De Pauw - Waterschoot';
+const sex = 'Male';
+const age = 36;
+const unicodeAvatar = '\u{1F344}';
+const married = false;
+const quote = 'I\'m a future talent in front-end development';
+const bmi = (65/(1.72*1.72));
+
+const message = `
+My personal information
+============================================================
+Firstname:\t${firstName}
+Surname:\t${surName}
+Sex:\t\t${sex}
+Age:\t\t${age}
+Married:\t${married}
+BMI:\t\t${bmi}
+Avatar:\t\t${unicodeAvatar}
+Quote:\t\t${quote}
+`;
+
+console.log(message);
